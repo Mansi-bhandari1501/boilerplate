1fc09ff3db2806f9d47d72383c9dc47543501f60
mans-bhandari1501 <mansi.bhandari1501@gmail.com>
2024-03-10 22:35:40 +0530

    first commit

---
+++ server/models/user.model.js
@@ -0,0 +1,155 @@
+import mongoose from "mongoose";
+
+const usersSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    Name: {
+        type: String,
+        // required: true
+        default: null
+    },
+    address: {
+        type: String,
+        city: {
+            type: String,
+            default: null
+          } ,
+          state: {
+            type: String,
+            default: null
+          },
+          country: {
+            type: String,
+            default: null
+          },
+    },
+    phone: {
+        type: String,
+        default: null
+    },
+    profileImage: {
+        type: String,
+        default: null
+    },
+    bgImage: {
+        type: String,
+        default: null
+    },
+    website:{
+        type: String,
+        default: null
+    },
+    company: {
+        type: Object,
+      name: {
+        type: String,
+        default: null
+      } ,
+      catchPhrase: {
+        type: String,
+        default: null
+      },
+      designation: {
+        type: String,
+        default: null
+      },
+      duration:{
+        type: Object,
+        start: {
+          type: String,
+          default: null
+        } ,
+        end: {
+          type: String,
+          default:"present"
+        },
+      }
+    
+    },
+    headline: {
+        type: String,
+        default: null
+    },
+    summary: {
+        type: String,
+        default: null
+    },
+    Education:[{
+
+      type: Object,
+        name: {
+          type: String,
+          default: null
+        } ,
+        catchPhrase: {
+          type: String,
+          default: null
+        },
+        designation: {
+          type: String,
+          default: null
+        },
+        duration:{
+            type: Object,
+            start: {
+              type: String,
+              default: null
+
+            } ,
+            end: {
+              type: String,
+              default:"present"
+            },
+        }
+    }],
+    skills:{
+
+        type: Object,
+      name: {
+        type: String,
+        default: null
+      } ,
+    }
+    
+
+   
+ // followers: [{
+    //     userid:
+    //     {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: "User"
+    //     },
+    //     status: {
+    //         type: String,
+    //         enum: ['pending', 'accepted', 'rejected'],
+    //         default : 'pending',
+    //     }
+    // }],
+    // following: [{
+    //     userid:
+    //     {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: "User"
+    //     },
+    //     status: {
+    //         type: String,
+    //         enum: ['pending', 'accepted', 'rejected'],
+    //         default : 'pending',
+    //     }
+
+    // }]
+    ,connection:{
+        type: String,
+        default: null
+        
+    }
+}, { timestamps: true })
+const UserModel = mongoose.model('Users', usersSchema);
+export default UserModel;
---
+++ server/routes/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import userRoutes from './user.routes.js';
+// import postRoutes from './post.routes.js';
+// import reactionRoutes from './reaction.routes.js';
+// import commentRoutes from './comment.routes.js';
+// import connectionRoutes from './connection.routes.js';
+// import messageRoutes from './message.routes.js';
+// import chatRoutes from './chat.routes.js';
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+  res.json({ message: "Welcome to mern app" });
+});
+
+router.use("/users",userRoutes );
+// router.use("/posts",postRoutes );
+// router.use("/comments",commentRoutes );
+// router.use("/reactions",reactionRoutes );
+// router.use("/connections",connectionRoutes );
+// router.use("/chats",chatRoutes );
+// router.use("/message",messageRoutes );
+
+export default router;
+
